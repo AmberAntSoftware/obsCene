0756e0f60e763c9f7a717e300f488cae3ec335b5
AmberCadence <@>
2019-05-20 03:18:56 -0400

    updated appropriate regions of data

--- obscene.c
+++ obscene.c
@@ -2,7 +2,22 @@
 
 #define DEREF_VOID(ptr) (*((void **) ( ptr ) ))
 
-void *traverseLinkedNodes(void *base, void *node){
+#define COMPARE__SWITCH_SET(comparator, exitLabel, type, charStorageResult, ptr1, ptr2) \
+switch( comparator ){\
+    COMPARE__SWITCH_UNIT( COMPARE_E_, exitLabel , type ,== , charStorageResult, ptr1 , ptr2);\
+    COMPARE__SWITCH_UNIT( COMPARE_L_, exitLabel , type , <, charStorageResult, ptr1, ptr2);\
+    COMPARE__SWITCH_UNIT( COMPARE_LE_, exitLabel , type , <= ,charStorageResult, ptr1 , ptr2);\
+    default:goto exitLabel;\
+}
+#define COMPARE__SWITCH_UNIT(switch_value, exitLabel, type, action, charStorageResult, ptr1, ptr2) \
+case(switch_value): \
+    charStorageResult = (*(type *) ptr1 ) action (*(type *) ptr2 );\
+    goto exitLabel;
+
+
+
+
+void *OBC_traverseLinkedNodes(void *base, void *node){
 	void *offset = (void*)((node-base)-1);
     void *accumulator = node;
     void *ret = node;
@@ -14,4 +29,67 @@ void *traverseLinkedNodes(void *base, void *node){
 	}
 
 	return ret;
+}
+
+
+
+
+#define COMPARE_END COMPARE_ACTION, COMPARE_ACTION_END
+
+void* insertLinkedList_(void *base, void *node, void *dataBaseToAdd, ...){
+    char* buffer;
+
+    int truth = 0;
+
+    int type = 0;
+    int comparator = 0;
+    char inversion = 0;
+    void *ptr1;
+    void *ptr2;
+    switch(type){
+        case(COMPARE_INT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed int, truth, ptr1, ptr2);
+        case(COMPARE_UINT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned int, truth, ptr1, ptr2);
+        case(COMPARE_FLOAT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, float, truth, ptr1, ptr2);
+        case(COMPARE_DOUBLE_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, double, truth, ptr1, ptr2);
+        case(COMPARE_CHAR_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed char, truth, ptr1, ptr2);
+        case(COMPARE_UCHAR_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned char, truth, ptr1, ptr2);
+        case(COMPARE_SHORT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed short, truth, ptr1, ptr2);
+        case(COMPARE_USHORT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned short, truth, ptr1, ptr2);
+        case(COMPARE_LLINT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed long long int, truth, ptr1, ptr2);
+        case(COMPARE_ULLINT_):
+                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned long long int, truth, ptr1, ptr2);
+        case(COMPARE_CHARSTRING_):{
+            //
+        }
+    }
+    EXIT_TYPES:
+
+    /*buffer = SDL_malloc(sizeof(char));
+    if(buffer==NULL){
+        return NULL;
+    }
+    buffer[0] = '\0';
+
+
+    va_list params;
+    va_start(params, char_pointer_count);
+    ///
+
+    int i;
+    for(i = 0; i < char_pointer_count;i++){
+        buffer = STR_concatf2(buffer,va_arg ( params, char* ));
+    }
+    ///
+    va_end(params);*/
+
+    return base;
 }
--- obscene.h
+++ obscene.h
@@ -1,6 +1,75 @@
 #ifndef OBSCENE_H_INCLUDED
 #define OBSCENE_H_INCLUDED
 
-void *traverseLinkedNodes(void *base, void *node);
+#include <stdarg.h>
+
+#ifndef NULL
+    #define NULL 0
+#endif // NULL
+
+void *OBC_traverseLinkedNodes(void *base, void *node);
+
+//uses a char* as the base, as the size is the smallest primitive type to support as many systems as possible -- 8bit systems and up
+//will throw a ptr casting warning in a compiler when the sizeof(char) is smaller than the sizeof(void *)
+void* insertLinkedList_(void *base, void *node, void *dataBaseToAdd, ...);
+
+enum{
+    COMPARE_ACTION,
+    COMPARE_ACTION_END,
+
+    COMPARE_FLOAT_,
+    COMPARE_DOUBLE_,
+    COMPARE_CHAR_,
+    COMPARE_UCHAR_,
+    COMPARE_SHORT_,
+    COMPARE_USHORT_,
+    COMPARE_INT_,
+    COMPARE_UINT_,
+    COMPARE_LLINT_,
+    COMPARE_ULLINT_,
+    COMPARE_CHARSTRING_,
+
+    COMPARE_L_,
+    COMPARE_G_,
+    COMPARE_E_,
+    COMPARE_LE_,
+    COMPARE_GE_,
+    COMPARE_NE_,
+
+    OBC_INSERT_,
+    OBC_PREPEND_,
+    OBC_APPEND_,
+    OBC_MIN_,
+    OBC_MAX_,
+    OBC_REVERSE_,
+
+    OBC_NODE_
+
+};
+
+#define COMPARE_FLOAT COMPARE_ACTION, COMPARE_FLOAT_
+#define COMPARE_DOUBLE COMPARE_ACTION, COMPARE_DOUBLE_
+#define COMPARE_CHAR COMPARE_ACTION, COMPARE_CHAR_
+#define COMPARE_UCHAR COMPARE_ACTION, COMPARE_UCHAR_
+#define COMPARE_SHORT COMPARE_ACTION, COMPARE_SHORT_
+#define COMPARE_USHORT COMPARE_ACTION, COMPARE_USHORT_
+#define COMPARE_INT COMPARE_ACTION, COMPARE_INT_
+#define COMPARE_UINT COMPARE_ACTION, COMPARE_UINT_
+#define COMPARE_LLINT COMPARE_ACTION, COMPARE_LLINT_
+#define COMPARE_ULLINT COMPARE_ACTION, COMPARE_ULLINT_
+#define COMPARE_CHARS8 COMPARE_ACTION, COMPARE_CHARSTRING_, 8
+#define COMPARE_CHARS16 COMPARE_ACTION, COMPARE_CHARSTRING_, 16
+#define COMPARE_CHARS24 COMPARE_ACTION, COMPARE_CHARSTRING_, 24
+#define COMPARE_CHARS32 COMPARE_ACTION, COMPARE_CHARSTRING_, 32
+
+#define COMPARE_L COMPARE_ACTION, COMPARE_L
+#define COMPARE_G COMPARE_ACTION, COMPARE_G
+#define COMPARE_E COMPARE_ACTION, COMPARE_E
+#define COMPARE_LE COMPARE_ACTION, COMPARE_LE
+#define COMPARE_GE COMPARE_ACTION, COMPARE_GE
+
+#define COMPARE_END COMPARE_ACTION, COMPARE_ACTION_END
+
+#define insertLinkedList(basePtr, nodePtr, dataBaseToAddPtr, args...) insertLinkedList_(base, node, dataBaseToAddPtr, args, COMPARE_END)
 
 #endif // OBSCENE_H_INCLUDED
