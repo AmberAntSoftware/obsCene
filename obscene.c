320ce5bc4139b8b57ae22f98278ae0ccd731941a
AmberCadence <@>
2019-05-22 03:40:42 -0400

    added and started full skeleton of program

--- obscene.c
+++ obscene.c
@@ -4,9 +4,9 @@
 
 #define COMPARE__SWITCH_SET(comparator, exitLabel, type, charStorageResult, ptr1, ptr2) \
 switch( comparator ){\
-    COMPARE__SWITCH_UNIT( COMPARE_E_, exitLabel , type ,== , charStorageResult, ptr1 , ptr2);\
-    COMPARE__SWITCH_UNIT( COMPARE_L_, exitLabel , type , <, charStorageResult, ptr1, ptr2);\
-    COMPARE__SWITCH_UNIT( COMPARE_LE_, exitLabel , type , <= ,charStorageResult, ptr1 , ptr2);\
+    COMPARE__SWITCH_UNIT( OBC_ACTION_COMPARE_E_, exitLabel , type ,== , charStorageResult, ptr1 , ptr2);\
+    COMPARE__SWITCH_UNIT( OBC_ACTION_COMPARE_L_, exitLabel , type , <, charStorageResult, ptr1, ptr2);\
+    COMPARE__SWITCH_UNIT( OBC_ACTION_COMPARE_LE_, exitLabel , type , <= ,charStorageResult, ptr1 , ptr2);\
     default:goto exitLabel;\
 }
 #define COMPARE__SWITCH_UNIT(switch_value, exitLabel, type, action, charStorageResult, ptr1, ptr2) \
@@ -34,62 +34,104 @@ void *OBC_traverseLinkedNodes(void *base, void *node){
 
 
 
-#define COMPARE_END COMPARE_ACTION, COMPARE_ACTION_END
 
-void* insertLinkedList_(void *base, void *node, void *dataBaseToAdd, ...){
+void* linkedListTraversal_(void *base, ...){
     char* buffer;
 
-    int truth = 0;
+    char truth = 0;
 
     int type = 0;
     int comparator = 0;
     char inversion = 0;
     void *ptr1;
     void *ptr2;
-    switch(type){
-        case(COMPARE_INT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed int, truth, ptr1, ptr2);
-        case(COMPARE_UINT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned int, truth, ptr1, ptr2);
-        case(COMPARE_FLOAT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, float, truth, ptr1, ptr2);
-        case(COMPARE_DOUBLE_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, double, truth, ptr1, ptr2);
-        case(COMPARE_CHAR_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed char, truth, ptr1, ptr2);
-        case(COMPARE_UCHAR_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned char, truth, ptr1, ptr2);
-        case(COMPARE_SHORT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed short, truth, ptr1, ptr2);
-        case(COMPARE_USHORT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned short, truth, ptr1, ptr2);
-        case(COMPARE_LLINT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed long long int, truth, ptr1, ptr2);
-        case(COMPARE_ULLINT_):
-                COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned long long int, truth, ptr1, ptr2);
-        case(COMPARE_CHARSTRING_):{
-            //
-        }
-    }
-    EXIT_TYPES:
 
-    /*buffer = SDL_malloc(sizeof(char));
-    if(buffer==NULL){
-        return NULL;
-    }
-    buffer[0] = '\0';
-
+    int argumentCount = 0;
 
     va_list params;
-    va_start(params, char_pointer_count);
+    va_start(params, base);
     ///
 
-    int i;
-    for(i = 0; i < char_pointer_count;i++){
-        buffer = STR_concatf2(buffer,va_arg ( params, char* ));
+    ///FIXME USE BETTER NAMES FOR ENDING TOKENS
+    ///OPTIMIZING ROUND -- combine node data here
+    while(1){
+        ptr1 = va_arg ( params, void* );
+
+        if(ptr1 == OBC_ACTION_){
+            ptr1 = va_arg ( params, void* );
+            switch((unsigned int)ptr1){
+                case(OBC_ACTION_END_):
+                    goto EXIT_PREPROCESS;
+                /*case(OBC_ACTION_NEXTNODE_):
+                    continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;
+                case():continue;*/
+                //
+                case(OBC_ACTION_NEXTNODE_):
+                    //process nodes
+                    continue;
+                case(OBC_ACTION_DATANODE_):
+                    //process nodes
+                    continue;
+
+            }
+        }
     }
+    EXIT_PREPROCESS:
+
+    printf("O K\n");
+    return NULL;
+
     ///
-    va_end(params);*/
+    va_end(params);
+
+    /**
+    ***
+    ***
+    ***
+    ***
+    **/
+
+    while(ptr2!=NULL){
+
+        switch(type){
+            case(OBC_ACTION_COMPARE_INT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed int, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_UINT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned int, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_FLOAT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, float, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_DOUBLE_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, double, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_CHAR_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed char, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_UCHAR_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned char, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_SHORT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed short, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_USHORT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned short, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_LLINT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, signed long long int, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_ULLINT_):
+                    COMPARE__SWITCH_SET(comparator, EXIT_TYPES, unsigned long long int, truth, ptr1, ptr2);
+            case(OBC_ACTION_COMPARE_CHARSTRING_):{
+                //
+            }
+        }
+        EXIT_TYPES:
+        ///FIXME DUMMY CODE
+        ptr1=ptr1;
+
+    }
 
     return base;
 }
